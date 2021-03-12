1bc68407b49fa6ee1b423f96620aba4e6c70eae6
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2021-03-12 17:11:59 +0500

    configtx initial release

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function createOrgYaml(){
 
     var orglist=[]
-    var total_org=document.querySelector("tbody").rows.length
-    var table=document.querySelector("tbody")
+    var peerorg=[]
+    var total_org=document.querySelector("#PeerTable tbody").rows.length
+    var table=document.querySelector("#PeerTable tbody")
 
     for (var i = 0; i < total_org; i++) {
 
@@ -12,6 +13,7 @@ function createOrgYaml(){
         var totalpeers=table.rows[i].cells[3].children[0].valueAsNumber;
         var sans=table.rows[i].cells[4].children[0].value;
         var totalusers=table.rows[i].cells[5].children[0].valueAsNumber;
+        var port=table.rows[i].cells[6].children[0].valueAsNumber;
 
         orglist.push({
             "PeerOrgs":
@@ -31,6 +33,39 @@ function createOrgYaml(){
             }
 
         })
+        orgmsp=orgname+"MSP"
+        peerorg.push(
+          {
+            "Name": orgmsp,
+            "ID": orgmsp,
+            "MSPDir": `../organizations/peerOrganizations/${orgdomain}/msp`,
+            "Policies": {
+              "Readers": {
+                "Type": "Signature",
+                "Rule": `OR(${orgmsp}.admin, ${orgmsp}.peer, ${orgmsp}.client)`
+              },
+              "Writers": {
+                "Type": "Signature",
+                "Rule": `OR(${orgmsp}.admin, ${orgmsp}.client)`
+              },
+              "Admins": {
+                "Type": "Signature",
+                "Rule": `OR('${orgmsp}.admin')`
+              },
+              "Endorsement": {
+                "Type": "Signature",
+                "Rule": `OR('${orgmsp}.peer')`
+              }
+            },
+            "AnchorPeers": [
+              {
+                "Host": "peer0."+orgdomain,
+                "Port": port
+              }
+            ]
+          }
+
+        )
 
     }
 
@@ -48,21 +83,299 @@ function createOrgYaml(){
         newlink.click();
         
     }
+    console.log(peerorg)
 
+    createConfigtx(peerorg)
 
 }
 
-function createConfigtx(){
+function createConfigtx(peerorg){
 
+  var total_Orderer=document.querySelector("#OrdererTable tbody").rows.length
+  var OrdererTable=document.querySelector("#OrdererTable tbody")
+
+
+
+    var orderer={
+
+            "Name": "OrdererOrg",
+            "ID": "OrdererMSP",
+            "MSPDir": "../organizations/ordererOrganizations/example.com/msp",
+            "Policies": {
+              "Readers": {
+                "Type": "Signature",
+                "Rule": "OR('OrdererMSP.member')"
+              },
+              "Writers": {
+                "Type": "Signature",
+                "Rule": "OR('OrdererMSP.member')"
+              },
+              "Admins": {
+                "Type": "Signature",
+                "Rule": "OR('OrdererMSP.admin')"
+              }
+            },
+            "OrdererEndpoints": [
+            ]
+    }
+   
+    for (let i = 0; i < total_Orderer; i++) {
+
+      var ordererdomain= OrdererTable.rows[i].cells[0].children[0].value;
+      var port=OrdererTable.rows[i].cells[1].children[0].valueAsNumber;
+        
+        orderer.OrdererEndpoints.push(ordererdomain+":"+port)
+    }
     
+    console.log(orderer.OrdererEndpoints)
    
     
 
     var configtx={
-        "Organizations" :[],
-
+      "Organizations": [
+      ],
+      "Capabilities": {
+        "Channel": {
+          "V2_0": true
+        },
+        "Orderer": {
+          "V2_0": true
+        },
+        "Application": {
+          "V2_0": true
+        }
+      },
+      "Application": {
+        "Organizations": null,
+        "Policies": {
+          "Readers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Readers"
+          },
+          "Writers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Writers"
+          },
+          "Admins": {
+            "Type": "ImplicitMeta",
+            "Rule": "MAJORITY Admins"
+          },
+          "LifecycleEndorsement": {
+            "Type": "ImplicitMeta",
+            "Rule": "MAJORITY Endorsement"
+          },
+          "Endorsement": {
+            "Type": "ImplicitMeta",
+            "Rule": "MAJORITY Endorsement"
+          }
+        },
+        "Capabilities": {
+          "V2_0": true
+        }
+      },
+      "Orderer": {
+        "OrdererType": "etcdraft",
+        "EtcdRaft": {
+          "Consenters": []
+        },
+        "Addresses": [],
+        "BatchTimeout": "2s",
+        "BatchSize": {
+          "MaxMessageCount": 10,
+          "AbsoluteMaxBytes": "99 MB",
+          "PreferredMaxBytes": "512 KB"
+        },
+        "Organizations": null,
+        "Policies": {
+          "Readers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Readers"
+          },
+          "Writers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Writers"
+          },
+          "Admins": {
+            "Type": "ImplicitMeta",
+            "Rule": "MAJORITY Admins"
+          },
+          "BlockValidation": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Writers"
+          }
+        }
+      },
+      "Channel": {
+        "Policies": {
+          "Readers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Readers"
+          },
+          "Writers": {
+            "Type": "ImplicitMeta",
+            "Rule": "ANY Writers"
+          },
+          "Admins": {
+            "Type": "ImplicitMeta",
+            "Rule": "MAJORITY Admins"
+          }
+        },
+        "Capabilities": {
+          "V2_0": true
+        }
+      },
+      "Profiles": {
+        "TwoOrgsOrdererGenesis": {
+          "Policies": {
+            "Readers": {
+              "Type": "ImplicitMeta",
+              "Rule": "ANY Readers"
+            },
+            "Writers": {
+              "Type": "ImplicitMeta",
+              "Rule": "ANY Writers"
+            },
+            "Admins": {
+              "Type": "ImplicitMeta",
+              "Rule": "MAJORITY Admins"
+            }
+          },
+          "Capabilities": {
+            "V2_0": true
+          },
+          "Orderer": {
+            "OrdererType": "etcdraft",
+            "EtcdRaft": {
+              "Consenters": []
+            },
+            "Addresses": [],
+            "BatchTimeout": "2s",
+            "BatchSize": {
+              "MaxMessageCount": 10,
+              "AbsoluteMaxBytes": "99 MB",
+              "PreferredMaxBytes": "512 KB"
+            },
+            "Organizations": [],
+            "Policies": {
+              "Readers": {
+                "Type": "ImplicitMeta",
+                "Rule": "ANY Readers"
+              },
+              "Writers": {
+                "Type": "ImplicitMeta",
+                "Rule": "ANY Writers"
+              },
+              "Admins": {
+                "Type": "ImplicitMeta",
+                "Rule": "MAJORITY Admins"
+              },
+              "BlockValidation": {
+                "Type": "ImplicitMeta",
+                "Rule": "ANY Writers"
+              }
+            },
+            "Capabilities": {
+              "V2_0": true
+            }
+          },
+          "Consortiums": {
+            "SampleConsortium": {
+              "Organizations": [ ]
+            }
+          }
+        },
+        "TwoOrgsChannel": {
+          "Consortium": "SampleConsortium",
+          "Policies": {
+            "Readers": {
+              "Type": "ImplicitMeta",
+              "Rule": "ANY Readers"
+            },
+            "Writers": {
+              "Type": "ImplicitMeta",
+              "Rule": "ANY Writers"
+            },
+            "Admins": {
+              "Type": "ImplicitMeta",
+              "Rule": "MAJORITY Admins"
+            }
+          },
+          "Capabilities": {
+            "V2_0": true
+          },
+          "Application": {
+            "Organizations": [],
+            "Policies": {
+              "Readers": {
+                "Type": "ImplicitMeta",
+                "Rule": "ANY Readers"
+              },
+              "Writers": {
+                "Type": "ImplicitMeta",
+                "Rule": "ANY Writers"
+              },
+              "Admins": {
+                "Type": "ImplicitMeta",
+                "Rule": "MAJORITY Admins"
+              },
+              "LifecycleEndorsement": {
+                "Type": "ImplicitMeta",
+                "Rule": "MAJORITY Endorsement"
+              },
+              "Endorsement": {
+                "Type": "ImplicitMeta",
+                "Rule": "MAJORITY Endorsement"
+              }
+            },
+            "Capabilities": {
+              "V2_0": true
+            }
+          }
+        }
+      }
     }
-    configtx.Organizations.push(org)
+
+    configtx.Organizations.push(orderer)
+    configtx.Organizations.push(peerorg)
+
+
+    var Consenters=[]
+    var Addresses=[]
+    for (let i = 0; i < total_Orderer; i++) {
+
+      var ordererdomain= OrdererTable.rows[i].cells[0].children[0].value;
+      var port=OrdererTable.rows[i].cells[1].children[0].valueAsNumber;
+
+      Consenters.push({
+
+        "Host": ordererdomain,
+        "Port": port,
+        "ClientTLSCert": `../organizations/ordererOrganizations/example.com/orderers/${ordererdomain}/tls/server.crt`,
+        "ServerTLSCert": `../organizations/ordererOrganizations/example.com/orderers/${ordererdomain}/tls/server.crt`
+
+      })
+
+      Addresses.push(ordererdomain+":"+port)
+      
+    }
+    console.log(Consenters)
+    console.log(Addresses)
+
+    configtx.Orderer.EtcdRaft.Consenters.push(Consenters)
+    configtx.Orderer.Addresses.push(Addresses)
+    
+    configtx.Profiles.TwoOrgsOrdererGenesis.Orderer.EtcdRaft.Consenters.push(Consenters)
+    configtx.Profiles.TwoOrgsOrdererGenesis.Orderer.Addresses.push(Addresses)
+    configtx.Profiles.TwoOrgsOrdererGenesis.Orderer.Organizations.push(orderer)
+
+    configtx.Profiles.TwoOrgsOrdererGenesis.Consortiums.SampleConsortium.Organizations.push(peerorg)
+
+    configtx.Profiles.TwoOrgsChannel.Application.Organizations.push(peerorg)
+
+    console.log(configtx)
+
+
+
 
     let yamlStr = jsyaml.safeDump(configtx)
     const newlink = document.createElement("a");
@@ -71,7 +384,7 @@ function createConfigtx(){
         type: "text/yaml"
     }));
 
-    newlink.setAttribute("download", `apple.yaml`);
+    newlink.setAttribute("download", `configtx.yaml`);
 
     newlink.click();
  
